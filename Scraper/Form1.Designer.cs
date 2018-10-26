f5a47bf5e6e4ffc367d910a83ea17f644aed0d7f
Petar Kostic <petarkostic1995@gmail.com>
2018-10-26 20:54:06 +0200

    Added the possibility to get info about different painters

--- Scraper/Form1.Designer.cs
+++ Scraper/Form1.Designer.cs
@@ -33,18 +33,18 @@
             this.button2 = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.button3 = new System.Windows.Forms.Button();
             this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 173);
+            this.button1.Location = new System.Drawing.Point(12, 227);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(343, 23);
             this.button1.TabIndex = 0;
@@ -54,7 +54,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(12, 12);
+            this.button2.Location = new System.Drawing.Point(12, 53);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(137, 23);
             this.button2.TabIndex = 1;
@@ -65,12 +65,10 @@
             // groupBox1
             // 
             this.groupBox1.Controls.Add(this.label4);
-            this.groupBox1.Controls.Add(this.label3);
-            this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 41);
+            this.groupBox1.Location = new System.Drawing.Point(12, 82);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(343, 74);
+            this.groupBox1.Size = new System.Drawing.Size(343, 50);
             this.groupBox1.TabIndex = 2;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Results";
@@ -78,29 +76,11 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 55);
+            this.label4.Location = new System.Drawing.Point(6, 29);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(105, 13);
             this.label4.TabIndex = 4;
             this.label4.Text = "Number of Paintings:";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 42);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(126, 13);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Date of death: 4-10-1669";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 29);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(125, 13);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Date of birth: 15-06-1606";
             // 
             // label1
             // 
@@ -113,14 +93,14 @@
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(155, 12);
+            this.progressBar1.Location = new System.Drawing.Point(155, 53);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(200, 23);
             this.progressBar1.TabIndex = 3;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(12, 121);
+            this.button3.Location = new System.Drawing.Point(12, 138);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(343, 23);
             this.button3.TabIndex = 4;
@@ -131,17 +111,37 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(12, 147);
+            this.label5.Location = new System.Drawing.Point(9, 164);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(56, 13);
             this.label5.TabIndex = 5;
             this.label5.Text = "Saved to: ";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(13, 23);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(30, 13);
+            this.label6.TabIndex = 6;
+            this.label6.Text = "Artist";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(49, 20);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(306, 21);
+            this.comboBox1.TabIndex = 7;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(383, 262);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.progressBar1);
@@ -163,13 +163,13 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
         private System.Windows.Forms.GroupBox groupBox1;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label4;
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox1;
     }
 }
 
--- Scraper/Form1.cs
+++ Scraper/Form1.cs
@@ -8,8 +8,6 @@ using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Support.UI;
 
-
-
 namespace Scraper
 {
     public partial class Form1 : Form
@@ -27,6 +25,20 @@ namespace Scraper
             progressBar1.Step = 1;
             progressBar1.Value = 0;
 
+            // Rembrandt van Rijn, Ferdinand bol, gerrit dou, carel fabritius, nicolaes maes en Samuel van hoogstraten
+            SortedDictionary<string, string> userCache = new SortedDictionary<string, string>
+            {
+                {"Rembrandt van Rijn", "https://www.wikiart.org/en/rembrandt/all-works/text-list"},
+                {"Ferdinand Bol", "https://www.wikiart.org/en/ferdinand-bol/all-works/text-list"},
+                {"Gerrit Dou", "https://www.wikiart.org/en/gerrit-dou/all-works/text-list"},
+                {"Carel Fabritius", "https://www.wikiart.org/en/carel-fabritius/all-works/text-list" },
+                {"Nicolaes Maes", "https://www.wikiart.org/en/nicolaes-maes/all-works/text-list"},
+                {"Samuel Dirksz van Hoogstraten", "https://www.wikiart.org/en/samuel-dirksz-van-hoogstraten/all-works/text-list" }
+            };
+
+            comboBox1.DataSource = new BindingSource(userCache, null);
+            comboBox1.DisplayMember = "Key";
+            comboBox1.ValueMember = "Value";
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -74,7 +86,7 @@ namespace Scraper
                     else
                         year = dataLi[1];
                     Console.WriteLine(listOfPaintingUrls[i].Text);
-                    paintings.Add(new Painting(i, "Rembrandt", "15-06-1606", "4-10-1669", dataLi[0], year, listOfPaintingUrls[i].GetAttribute("href")));
+                    paintings.Add(new Painting(i, comboBox1.Text, dataLi[0], year, listOfPaintingUrls[i].GetAttribute("href")));
                     LogPaintings(paintings);
                     progressBar1.Value = i;
                 }
@@ -84,12 +96,17 @@ namespace Scraper
                 MessageBox.Show("Something went wrong with finding the appropriate paintings! Contact Petar :)");
         }
 
+
+        /// <summary>
+        /// Provies a csv-like console writeline loop for the list of Paintings currently in working
+        /// memory.
+        /// </summary>
+        /// <param name="list">The list to be printed</param>
         private void LogPaintings(List<Painting> list)
         {
-            Console.WriteLine("name;type;businesstype;industrytype");
+            Console.WriteLine("id;artist;namepainting;yearpainting;url");
             foreach (var p in paintings)
-                Console.WriteLine(p.id + ";" + p.artist + ";" + p.artistBirthDate + ";" + p.artistDeathDate + ";" +
-                                  p.namePainting + ";" + p.yearPainting + ";" + p.url);
+                Console.WriteLine(p.id + ";" + p.artist + ";" + p.namePainting + ";" + p.yearPainting + ";" + p.url);
         }
 
         /// <summary>
@@ -101,54 +118,59 @@ namespace Scraper
         {
             if (paintings.Count > 0)
             {
-                //foreach (var p in paintings)
-                // {
-                // }
-
-                // As a test, navigate to the url of the first painting
-                driver.Navigate().GoToUrl(paintings[0].url);
-                var imageSrc = driver
-                    .FindElement(By.XPath("/html/body/div[2]/div[1]/section[1]/main/div[2]/aside/div[1]/img"))
-                    .GetAttribute("src");
-
                 // Create a new folder for the run
                 string path = @"C:\" + DateTime.Now.ToString("dd-MM-yyyy h-mm-tt");
                 Directory.CreateDirectory(path);
 
-                // Create an HTTP web request to feed the binary stream reader
-                byte[] imageBytes;
-                HttpWebRequest imageRequest = (HttpWebRequest)WebRequest.Create(imageSrc);
-                WebResponse imageResponse = imageRequest.GetResponse();
+                foreach (var p in paintings)
+                {
+                    // As a test, navigate to the url of the first painting
+                    driver.Navigate().GoToUrl(p.url);
+                    var imageSrc = driver
+                        .FindElement(By.XPath("/html/body/div[2]/div[1]/section[1]/main/div[2]/aside/div[1]/img"))
+                        .GetAttribute("src");
 
-                Stream responseStream = imageResponse.GetResponseStream();
+                    // Create an HTTP web request to feed the binary stream reader
+                    byte[] imageBytes;
+                    HttpWebRequest imageRequest = (HttpWebRequest)WebRequest.Create(imageSrc);
+                    WebResponse imageResponse = imageRequest.GetResponse();
 
-                using (BinaryReader br = new BinaryReader(responseStream))
-                {
-                    imageBytes = br.ReadBytes(500000);
-                    br.Close();
-                }
-                responseStream.Close();
-                imageResponse.Close();
-                string imageFinalFileName = path + '\\' + paintings[0].id + ".jpg";
-                FileStream fs = new FileStream(imageFinalFileName, FileMode.Create);
-                BinaryWriter bw = new BinaryWriter(fs);
-                try
-                {
-                    bw.Write(imageBytes);
-                }
-                finally
-                {
-                    fs.Close();
-                    bw.Close();
+                    Stream responseStream = imageResponse.GetResponseStream();
+
+                    using (BinaryReader br = new BinaryReader(responseStream))
+                    {
+                        imageBytes = br.ReadBytes(500000);
+                        br.Close();
+                    }
+                    responseStream.Close();
+                    imageResponse.Close();
+                    string imageFinalFileName = path + '\\' + p.id + ".jpg";
+                    FileStream fs = new FileStream(imageFinalFileName, FileMode.Create);
+                    BinaryWriter bw = new BinaryWriter(fs);
+                    try
+                    {
+                        bw.Write(imageBytes);
+                    }
+                    finally
+                    {
+                        fs.Close();
+                        bw.Close();
+                    }
                 }
 
                 label5.Text = "Saved to: " + path;
             }
             else
-            {
                 MessageBox.Show("No paintings in working memory, click on 'Find Paintings' first.");
-            }
-
+        }
+  
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Retrieve the value from the currently selected key-value dictionary in the combobox
+            // and nagivate to that webpage.
+            var selectedUrl = ((KeyValuePair<string, string>)comboBox1.SelectedItem).Value;
+            driver.Navigate().GoToUrl(selectedUrl);
+            label1.Text = ((KeyValuePair<string, string>)comboBox1.SelectedItem).Key;
         }
     }
 
@@ -156,19 +178,15 @@ namespace Scraper
     {
         public int id;
         public string artist;
-        public string artistBirthDate;
-        public string artistDeathDate;
         public string namePainting;
         public string yearPainting;
         public string url;
 
-        public Painting(int id, string artist, string artistBirthDate, string artistDeathDate, string namePainting,
+        public Painting(int id, string artist, string namePainting,
             string yearPainting, string url)
         {
             this.id = id;
             this.artist = artist;
-            this.artistBirthDate = artistBirthDate;
-            this.artistDeathDate = artistDeathDate;
             this.namePainting = namePainting;
             this.yearPainting = yearPainting;
             this.url = url;
